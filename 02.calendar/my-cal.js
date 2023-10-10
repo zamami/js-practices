06c8eddb234d1cd4ac68f95a44b7609e32204f07
zamami <s1413115@gmail.com>
2023-10-10 15:20:27 +0900

    変数の型を変更、参考演算子から論理判別に変更

--- 02.calendar/my-cal.js
+++ 02.calendar/my-cal.js
@@ -1,13 +1,11 @@
-// for (let i = 0; i < process.argv.length; ++i) {
-//     console.log(i + ': ' + process.argv[i]);
-// }
-//
-// console.log(process.argv[3])
+
 
 import minimist from 'minimist';
 
 const argv = minimist(process.argv.slice(2));
-let month = ( argv['m'] ) ? argv['m'] : new Date().getMonth() + 1;
-let year = ( argv['y'] ) ? argv['y'] : new Date().getFullYear() ;
+// const month = ( argv['m'] ) ? argv['m'] : new Date().getMonth() + 1;
+const month = argv['m'] || new Date().getMonth() + 1;
+const year = argv['y'] || new Date().getFullYear();
+// const year = ( argv['y'] ) ? argv['y'] : new Date().getFullYear() ;
 console.log(month);
 console.log(year);
