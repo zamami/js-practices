79d3a9ba937908073b15659bf0e2f6b8c0e14238
zamami <s1413115@gmail.com>
2023-10-11 16:10:53 +0900

    ESLint,Prettier実行、修正済み

--- 02.calendar/my-cal.js
+++ 02.calendar/my-cal.js
@@ -1,17 +1,19 @@
-import minimist from 'minimist';
+import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
-const month = argv['m'] || new Date().getMonth() + 1;
-const year = argv['y'] || new Date().getFullYear();
+const month = argv["m"] || new Date().getMonth() + 1;
+const year = argv["y"] || new Date().getFullYear();
 
 // 月末日を取得
-const lastDate = new Date(year, month,0).getDate();
+const lastDate = new Date(year, month, 0).getDate();
 
 // 月の期間を作成
 let days = [];
 for (let i = 1; i < lastDate + 1; i++) {
-    if (i < 10) { i = " " + i.toString();} // 1~9の見た目調整
-    days.push(i);
+  if (i < 10) {
+    i = " " + i.toString();
+  } // 1~9の見た目調整
+  days.push(i);
 }
 
 // 月初の曜日を取得（0から6の整数)
@@ -19,14 +21,14 @@ const firstDayOfMonth = new Date(year, month - 1, 1);
 const dayOfWeek = firstDayOfMonth.getDay();
 
 // 曜日のスタート位置の分だけ、空白をdaysに入れる
-for(let i = 0; i < dayOfWeek; i++){
-    days.unshift("  ")
+for (let i = 0; i < dayOfWeek; i++) {
+  days.unshift("  ");
 }
 
 console.log(`       ${month}月 ${year}\n日 月 火 水 木 金 土`);
 
 const oneWeek = 7; // ７日ごとに区切り、要素をくっつけて表示
 for (let i = 0; i < days.length; i += oneWeek) {
-    const week = days.slice(i, i + oneWeek);
-    console.log(week.join(" "));
+  const week = days.slice(i, i + oneWeek);
+  console.log(week.join(" "));
 }
