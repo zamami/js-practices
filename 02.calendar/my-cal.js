3f02466f21dbd836f2d377ff947ba31088ff83e2
zamami <s1413115@gmail.com>
2023-10-13 21:03:12 +0900

    daysの要素をstringに統一。変数名を変更。出力部分ををconsole.logに修正

--- 02.calendar/my-cal.js
+++ 02.calendar/my-cal.js
@@ -9,12 +9,12 @@ const year = argv.y || now.getFullYear();
 const lastDate = new Date(year, month, 0).getDate();
 
 // 月の期間を作成
-const days = [];
+const formattedDays = [];
 for (let i = 1; i < lastDate + 1; i++) {
   if (i < 10) {
     i = " " + i.toString();
   } // 1~9の見た目調整
-  days.push(i);
+  formattedDays.push(i.toString());
 }
 
 // 月初の曜日を取得（0から6の整数)
@@ -22,14 +22,15 @@ const dayOfWeek = (new Date(year, month - 1, 1)).getDay();
 
 // 曜日のスタート位置の分だけ、空白をdaysに入れる
 for (let i = 0; i < dayOfWeek; i++) {
-  days.unshift("  ");
+  formattedDays.unshift("  ");
 }
 
-console.log(`       ${month}月 ${year}\n日 月 火 水 木 金 土`);
+console.log(`       ${month}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
 
 const oneWeek = 7; // ７日ごとに区切り、要素をくっつけて表示
-for (let i = 0; i < days.length; i += oneWeek) {
-  const week = days.slice(i, i + oneWeek);
+for (let i = 0; i < formattedDays.length; i += oneWeek) {
+  const week = formattedDays.slice(i, i + oneWeek);
   console.log(week.join(" "));
 }
 process.stdout.write("\n");
