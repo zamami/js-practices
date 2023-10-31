03939f28e98850160f85dac4d6c2eee8d709aea5
zamami <s1413115@gmail.com>
2023-10-31 15:28:12 +0900

    callbackのエラーなしファイルを作成

---
+++ 03.asynchronous/callback_no_error.js
@@ -0,0 +1,18 @@
+import sqlite3 from 'sqlite3';
+const db = new (sqlite3.verbose().Database)(":memory:");
+
+db.run("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",() => {
+    db.run("INSERT INTO books(title) VALUES (?)","callbackエラーなし",
+        function(){
+            console.log(`ID番号${this.lastID}`);
+            db.get("SELECT * FROM books WHERE id = ?",[this.lastID],(err, row) => {
+                console.log(row);
+                db.run("DROP TABLE books", () => {
+                    db.close();
+                });
+            });
+        }
+    );
+});
+
+
