3bb0706ccce26e49b1b9722aa727ec827e3cd152
zamami <s1413115@gmail.com>
2023-10-31 15:47:29 +0900

    npm rum fix実行済み

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,24 +1,31 @@
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 const db = new (sqlite3.verbose().Database)(":memory:");
 
-db.run("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",() => {
-    db.run("INSERT INTO books(title) VALUES (?)","callbackエラーあり",
-        function(err){
-            if(err){
-                console.log(err.message);
-            }
-            console.log(`ID番号${this.lastID}`);
-            db.get("SELECT * FROM books WHERE id = ?",[this.lastID],(err, row) => {
-                if(err){
-                    console.log(err.message);
-                }
-                console.log(row);
-                db.run("DROP TABLE books", () => {
-                    db.close();
-                });
-            });
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
+  () => {
+    db.run(
+      "INSERT INTO books(title) VALUES (?)",
+      "callbackエラーあり",
+      function (err) {
+        if (err) {
+          console.log(err.message);
         }
+        console.log(`ID番号${this.lastID}`);
+        db.get(
+          "SELECT * FROM books WHERE id = ?",
+          [this.lastID],
+          (err, row) => {
+            if (err) {
+              console.log(err.message);
+            }
+            console.log(row);
+            db.run("DROP TABLE books", () => {
+              db.close();
+            });
+          }
+        );
+      }
     );
-});
-
-
+  }
+);
