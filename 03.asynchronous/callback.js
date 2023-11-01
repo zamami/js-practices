bd56ed37cbd3ac40e71acf752ae0032732a8ddd5
zamami <s1413115@gmail.com>
2023-11-01 15:09:05 +0900

    エラーの処理をtry~catchに修正 ※npm rum fix実行済み

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -3,28 +3,35 @@ const db = new (sqlite3.verbose().Database)(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
-  () => {
+  function (err) {
+    if (err) {
+      return console.log(err.message);
+    }
     db.run(
       "INSERT INTO books(title) VALUES (?)",
-      "callbackエラーあり",
+      ["callbackエラーあり"],
       function (err) {
-        if (err) {
-          console.log(err.message);
-        }
-        console.log(`ID番号${this.lastID}`);
-        db.get(
-          "SELECT * FROM books WHERE id = ?",
-          [this.lastID],
-          (err, row) => {
-            if (err) {
-              console.log(err.message);
-            }
-            console.log(row);
-            db.run("DROP TABLE books", () => {
-              db.close();
-            });
+        try {
+          if (err) {
+            throw new Error(err.message);
           }
-        );
+          console.log(`ID番号${this.lastID}`);
+          db.get(
+            "SELECT * FROM books WHERE id = ?",
+            [this.lastID],
+            (err, row) => {
+              if (err) {
+                throw new Error(err.message);
+              }
+              console.log(row);
+              db.run("DROP TABLE books", () => {
+                db.close();
+              });
+            }
+          );
+        } catch (e) {
+          console.log(e.message);
+        }
       }
     );
   }
