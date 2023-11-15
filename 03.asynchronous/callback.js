fda8c07e136ed88a8c59734cbbf183150bdf6c8f
zamami <s1413115@gmail.com>
2023-11-15 21:45:22 +0900

    ・try...catch文の削除 ・INSERTとSELECTのみエラー対応

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -2,37 +2,38 @@ import sqlite3 from "sqlite3";
 const db = new (sqlite3.verbose().Database)(":memory:");
 
 db.run(
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
-  function (err) {
-    if (err) {
-      return console.log(err.message);
-    }
-    db.run(
-      "INSERT INTO books(title) VALUES (?)",
-      ["callbackエラーあり"],
-      function (err) {
-        try {
-          if (err) {
-            throw new Error(err.message);
-          }
-          console.log(`ID番号${this.lastID}`);
-          db.get(
-            "SELECT * FROM books WHERE id = ?",
-            [this.lastID],
-            (err, row) => {
-              if (err) {
-                throw new Error(err.message);
-              }
-              console.log(row);
-              db.run("DROP TABLE books", () => {
-                db.close();
-              });
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
+    () => {
+        db.run(
+            "INSERT INTO books(title) VALUES (?)",
+            null,  // NOT NULL 発生
+
+            function (err) {
+                if (err) {
+                    console.error(err.message);
+                    return;
+                }
+                console.log(`ID番号${this.lastID}`);
+                db.get(
+                    "SELECT * FROM books WHERE id = ?",
+                    [this.lastID],
+                    (err, row) => {
+                        if (err) {
+                            console.error(err.message);
+                            return;
+                        }
+                        console.log(row);
+                        //no such table 発生
+                        db.run("DROP TABLE hogehoge", (err) => {
+                            if (err) {
+                                console.error(err.message);
+                                return;
+                            }
+                            db.close();
+                        });
+                    }
+                );
             }
-          );
-        } catch (e) {
-          console.log(e.message);
-        }
-      }
-    );
-  }
+        );
+    }
 );
