7444ed9fb6968a76c69e19bf8ba66a308d03f3fb
zamami <s1413115@gmail.com>
2023-12-04 21:23:07 +0900

    全てのプロパティを扱えるようにthisを持たせるように修正。

--- 03.asynchronous/promise/promise_no_error.js
+++ 03.asynchronous/promise/promise_no_error.js
@@ -1,22 +1,4 @@
-import sqlite3 from "sqlite3";
-
-const db = new (sqlite3.verbose().Database)(":memory:");
-
-const runQuery = (query, params = []) => {
-  return new Promise((resolve) => {
-    db.run(query, params, function () {
-      resolve(this.lastID);
-    });
-  });
-};
-
-const getQuery = (query, params = []) => {
-  return new Promise((resolve) => {
-    db.get(query, params, (_, row) => {
-      resolve(row);
-    });
-  });
-};
+import { db, runQuery, getQuery} from "./promise_module.js";
 
 runQuery(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )"
@@ -24,9 +6,9 @@ runQuery(
   .then(() =>
     runQuery("INSERT INTO books(title) VALUES (?)", ["Promiseエラーなし"])
   )
-  .then((lastID) => {
-    console.log(`ID番号${lastID}`);
-    return getQuery("SELECT * FROM books WHERE id = ?", [lastID]);
+  .then((result) => {
+    console.log(`ID番号${result.lastID}`);
+    return getQuery("SELECT * FROM books WHERE id = ?", [result.lastID]);
   })
   .then((row) => {
     console.log(row);
