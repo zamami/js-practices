50b039bb6a449329c69ebc4d6673d46f6aec7e3a
zamami <s1413115@gmail.com>
2023-12-13 19:40:15 +0900

    改行追加。 コールバックのファイルのスタイルを統一しました。

--- 03.asynchronous/callback/callback.js
+++ 03.asynchronous/callback/callback.js
@@ -1,7 +1,6 @@
 import sqlite3 from "sqlite3";
 
 const db = new (sqlite3.verbose().Database)(":memory:");
-
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
